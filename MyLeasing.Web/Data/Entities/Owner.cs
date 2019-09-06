461f4f8ac6a1c2b2cb0b0f83a964f5e7ad8fc4ff
Danny Rojas Reyes <afarable-1997@hotmail.com>
2019-09-06 17:23:12 -0500

    Empty commit

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [Display(Name = "First Name")]
+        public string FirtName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
